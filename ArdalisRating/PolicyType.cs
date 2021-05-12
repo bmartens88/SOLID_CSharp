4c63949f4d3b6242841eaea74b4659958090902b
Bas Martens <bmartens1988@kpnmail.nl>
2021-05-12 11:29:47 +0200

    Initial commit of code

---
+++ ArdalisRating/PolicyType.cs
@@ -0,0 +1,9 @@
+﻿namespace ArdalisRating
+{
+    public enum PolicyType
+    {
+        Life = 0,
+        Land = 1,
+        Auto = 2
+    }
+}
---
+++ ArdalisRating/RatingEngine.cs
@@ -0,0 +1,87 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.IO;
+
+namespace ArdalisRating
+{
+    public class RatingEngine
+    {
+        public decimal Rating { get; set; }
+        public void Rate()
+        {
+            Console.WriteLine("Starting rate.");
+
+            Console.WriteLine("Loading policy");
+
+            string policyJson = File.ReadAllText("policy.json");
+
+            var policy = JsonConvert.DeserializeObject<Policy>(policyJson,
+                new StringEnumConverter());
+
+            switch (policy.Type)
+            {
+                case PolicyType.Auto:
+                    Console.WriteLine("Rating AUTO policy...");
+                    Console.WriteLine("Validating policy.");
+                    if (string.IsNullOrEmpty(policy.Make))
+                    {
+                        Console.WriteLine("Auto policy must specify Make");
+                        return;
+                    }
+                    if (policy.Make == "BMW")
+                    {
+                        Rating = policy.Dedutible < 500 ? 1000m : 900m;
+                    }
+                    break;
+
+                case PolicyType.Land:
+                    Console.WriteLine("Rating LAND policy...");
+                    Console.WriteLine("Validating policy.");
+                    if (policy.BondAmount == 0 || policy.Valuation == 0)
+                    {
+                        Console.WriteLine("Land policy must specify Bond Amound and Valuation");
+                        return;
+                    }
+                    if (policy.BondAmount < 0.8m * policy.Valuation)
+                    {
+                        Console.WriteLine("Insufficient bond amount.");
+                        return;
+                    }
+                    Rating = policy.BondAmount * 0.05m;
+                    break;
+
+                case PolicyType.Life:
+                    Console.WriteLine("Rating LIFE policy...");
+                    Console.WriteLine("Validating policy.");
+                    if (policy.DateOfBirth == DateTime.MinValue)
+                    {
+                        Console.WriteLine("Life policy must include Date Of Birth");
+                        return;
+                    }
+                    if (policy.DateOfBirth < DateTime.Today.AddYears(-100))
+                    {
+                        Console.WriteLine("Centenarians are not eligible for coverage");
+                        return;
+                    }
+                    int age = DateTime.Today.Year - policy.DateOfBirth.Year;
+                    if (policy.DateOfBirth.Month == DateTime.Today.Month &&
+                        DateTime.Today.Day < policy.DateOfBirth.Day ||
+                        DateTime.Today.Month < policy.DateOfBirth.Month)
+                    {
+                        age--;
+                    }
+                    decimal baseRate = policy.Amount * age / 200;
+                    if (policy.IsSmoker)
+                    {
+                        Rating = baseRate * 2;
+                        break;
+                    }
+                    Rating = baseRate;
+                    break;
+            }
+
+            Console.WriteLine("Rating completed.");
+        }
+    }
+}
