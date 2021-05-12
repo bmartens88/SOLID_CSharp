3744c6481f05f8a1b3479b5cfa46cbd208ebeacf
Bas Martens <bmartens1988@kpnmail.nl>
2021-05-12 16:04:02 +0200

    Halfway through applying OCP

---
+++ ArdalisRating/AutoPolicyRater.cs
@@ -0,0 +1,23 @@
+﻿namespace ArdalisRating
+{
+    public class AutoPolicyRater : Rater
+    {
+        public AutoPolicyRater(RatingEngine engine, ConsoleLogger logger)
+            : base(engine, logger) { }
+
+        public override void Rate(Policy policy)
+        {
+            _logger.Log("Rating AUTO policy...");
+            _logger.Log("Validating policy.");
+            if (string.IsNullOrEmpty(policy.Make))
+            {
+                _logger.Log("Auto policy must specify Make");
+                return;
+            }
+            if (policy.Make == "BMW")
+            {
+                _engine.Rating = policy.Dedutible < 500 ? 1000m : 900m;
+            }
+        }
+    }
+}
--- ArdalisRating/RatingEngine.cs
+++ ArdalisRating/RatingEngine.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace ArdalisRating
+﻿namespace ArdalisRating
 {
     public class RatingEngine
     {
@@ -21,67 +19,10 @@ namespace ArdalisRating
 
             var policy = PolicySerializer.GetPolicyFromJsonString(policyJson);
 
-            switch (policy.Type)
-            {
-                case PolicyType.Auto:
-                    Logger.Log("Rating AUTO policy...");
-                    Logger.Log("Validating policy.");
-                    if (string.IsNullOrEmpty(policy.Make))
-                    {
-                        Logger.Log("Auto policy must specify Make");
-                        return;
-                    }
-                    if (policy.Make == "BMW")
-                    {
-                        Rating = policy.Dedutible < 500 ? 1000m : 900m;
-                    }
-                    break;
+            var factory = new RaterFactory();
 
-                case PolicyType.Land:
-                    Logger.Log("Rating LAND policy...");
-                    Logger.Log("Validating policy.");
-                    if (policy.BondAmount == 0 || policy.Valuation == 0)
-                    {
-                        Logger.Log("Land policy must specify Bond Amound and Valuation");
-                        return;
-                    }
-                    if (policy.BondAmount < 0.8m * policy.Valuation)
-                    {
-                        Logger.Log("Insufficient bond amount.");
-                        return;
-                    }
-                    Rating = policy.BondAmount * 0.05m;
-                    break;
-
-                case PolicyType.Life:
-                    Logger.Log("Rating LIFE policy...");
-                    Logger.Log("Validating policy.");
-                    if (policy.DateOfBirth == DateTime.MinValue)
-                    {
-                        Logger.Log("Life policy must include Date Of Birth");
-                        return;
-                    }
-                    if (policy.DateOfBirth < DateTime.Today.AddYears(-100))
-                    {
-                        Logger.Log("Centenarians are not eligible for coverage");
-                        return;
-                    }
-                    int age = DateTime.Today.Year - policy.DateOfBirth.Year;
-                    if (policy.DateOfBirth.Month == DateTime.Today.Month &&
-                        DateTime.Today.Day < policy.DateOfBirth.Day ||
-                        DateTime.Today.Month < policy.DateOfBirth.Month)
-                    {
-                        age--;
-                    }
-                    decimal baseRate = policy.Amount * age / 200;
-                    if (policy.IsSmoker)
-                    {
-                        Rating = baseRate * 2;
-                        break;
-                    }
-                    Rating = baseRate;
-                    break;
-            }
+            var rater = factory.Create(policy, this);
+            rater.Rate(policy);
 
             Logger.Log("Rating completed.");
         }
